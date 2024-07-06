f38ac8f25485a4aefc4345e80f2ae5d3eb35ac69
MoaazAbouobaida <moazabuobida17ca@gmail.com>
2024-07-06 14:47:39 +0300

    Adding Game Files

--- MoazAbuobida/Program.cs
+++ MoazAbuobida/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,20 +12,43 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".unityweb"] = "application/octet-stream";
+provider.Mappings[".data"] = "application/octet-stream";
+provider.Mappings[".wasm"] = "application/wasm";
+provider.Mappings[".json"] = "application/json";
+provider.Mappings[".js"] = "application/javascript";
+
+
+// Serve static files from wwwroot by default
+app.UseStaticFiles(new StaticFileOptions
+{
+	ContentTypeProvider = provider
+});
+
+
 app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+	FileProvider = new PhysicalFileProvider(@"D:\Unity\Projects\OmarJuniorDinoGame\WebGL"),
+	RequestPath = "/Game",
+	ContentTypeProvider = provider
+});
 
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
